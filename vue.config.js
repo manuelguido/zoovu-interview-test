e14f160f216484960c25f20526935eee4365fc92
manuelguido <manuelguido.m@gmail.com>
2023-03-07 13:48:18 -0300

    Updating production path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  publicPath: '/'
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/'
-  //   : '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'zoovu-interview-test'
+    : '/'
 }
