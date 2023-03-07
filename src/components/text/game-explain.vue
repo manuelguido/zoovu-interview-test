c888773d5f5a8cf1aedfd546b861bbb63cde9a74
manuelguido <manuelguido.m@gmail.com>
2023-03-07 12:36:30 -0300

    Adding game explanation to home page

---
+++ src/components/text/game-explain.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="game-explain py-5">
+    <p class="mb-0">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextGameExplain",
+  data() {
+    return {
+      text: 'In order to win the game, you have to put all the cards in the correct order. If you miss one 10 seconds will be added to the timer. Complete it as fast as possible to get a better score!',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.game-explain {
+  position: absolute;
+  bottom: 0;
+}
+
+.game-explain p {
+  font-size: 1.25rem;
+  color: #aaa;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,12 @@
   <div
     class="height-100 d-flex flex-column align-items-center justify-content-center"
   >
-    <!-- Title -->
     <h1 class="h2 uns mb-5">Hello friend, tell me your name...</h1>
     <!-- Name input -->
     <name-input v-model="username" />
     <!-- Submit button -->
     <submit-button @submitData="submit" :disabled="submitDisabled" />
+      <GameExplain/>
   </div>
   <!-- /.Container -->
 </template>
@@ -16,11 +16,14 @@
 <script>
 import NameInput from "../components/NameInput.vue";
 import SubmitButton from "../components/SubmitButton.vue";
+import GameExplain from '../components/text/game-explain.vue';
+
 export default {
   name: "HomePage",
   components: {
     NameInput,
     SubmitButton,
+    GameExplain,
   },
   data() {
     return {
