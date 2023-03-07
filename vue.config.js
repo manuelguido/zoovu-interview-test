fe5f94c3fbcb84d754b2874ac2fbabdf04a438ec
manuelguido <manuelguido.m@gmail.com>
2023-03-07 13:58:59 -0300

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: 'https://github.com/manuelguido/zoovu-interview-test/'
+  publicPath: '/'
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/zoovu-interview-test/'
   //   : '/'
